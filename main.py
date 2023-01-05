e1510a8c9bbec8841c01f3996a04b447f2f74968
Prakhar-Gupta-03 <prakhar21550@iiitd.ac.in>
2023-01-05 13:22:41 +0400

    basic window structure

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from tkinter import *
 from tkinter import ttk
 
 window = Tk()
-window.title("Algorithms Visualizer")
-window.configure(background = "grey");
-firstLabel = Label(window, text="First Label")
-firstLabel.grid(row = 0, column = 0)
-secondLabel = Label(window, text = "Second  Label")
-secondLabel.grid(row = 1, column = 0)
+window.title("Algorithm Visualizer")
+window.configure(background = "grey")
+def choose_algorithm():
+    print(selected_algorithm.get())
+#define a list of algorithms
+algorithms = ["Binary Search", "Kadane's Algorithm", "Selection Sort"]
+# holds the seleted algorithm
+selected_algorithm = StringVar()
+# set the default value
+selected_algorithm.set(algorithms[0])
+drop_down = OptionMenu(window, selected_algorithm, *algorithms)
+#defining a label
+choose_algorithm_label = Label(window, text = "Choose Algorithm:").grid(row = 0, column = 0, padx = 10, pady = 10)
+drop_down.grid(row = 0, column = 1, padx = 10, pady = 10)
+choose_algorithm = Button(window, text = "Choose Algorithm", command = choose_algorithm)
+choose_algorithm.grid(row = 1, column = 0, padx = 10, pady = 10)
 window.mainloop()
